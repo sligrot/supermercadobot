198989b5d75f4c313e962bf49cb6e4c9322d75db
remde <ffremde@gmail.com>
2021-02-10 09:04:43 -0300

    Add service functions

--- src/service.js
+++ src/service.js
@@ -1,27 +1,57 @@
 class Service {
-    list = []; 
+    list = [];
+    backupList = [];
 
     addItemToList = (message) => {
-        const item = this.getMessageBody(message, '+') 
-        this.list.push(item);
+        const item = this.getMessageBody(message, '+');
+        const itemArray = item.split(',');
+        for (let individualItem of itemArray) {
+            this.list.push(individualItem);
+        }
         return item
     }
 
-    getList = () => {
-        if (this.list === []) {
-            return ["A lista está vazia!"];
-        }    
-        return this.list; 
+    removeItemFromList = (message) => {
+        const item = this.getMessageBody(message, '-');
+        const index = this.list.indexOf(item);
+        if (index > -1) {
+            this.list.splice(index, 1);
+            return item;
+        }
+        throw error;
+    }
+
+    getFormattedList = () => {
+        if (this.list.length == 0) {
+            return "A lista está vazia! Adicione itens digitando '+ item'";
+        }
+        let formattedList = "Lista de Compras: \n";
+        for (let index in this.list) {
+            formattedList += "-" + this.list[index] + "\n";
+        } 
+        return formattedList;
     }
 
     deleteList = () => {
+        this.backupList = this.list;
         this.list = [];
+    }
+
+    getBackupList = () => {
+        if (this.backupList.length == 0) {
+            return "A lista antiga está vazia";
+        }
+        let formattedList = "A última lista de compras foi: \n";
+        for (let index in this.backupList) {
+            formattedList += "-" + this.backupList[index] + "\n";
+        } 
+        return formattedList
     }
 
     getMessageBody = (message, token) => {
         for (let i=0; i<message.length; i++) {
             if (message[i] == token) {
-                return message.slice([i+2]);
+                return message.slice([i+1]);
             }
         }
     }
